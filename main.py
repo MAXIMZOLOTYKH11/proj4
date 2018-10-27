82927d003ac10d6e68e8e465695b0e0bffdcc2d3
MAXIMZOLOTYKH11 <max.zl@mail.ru>
2018-10-27 17:47:00 +0700

    initial commit3

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-# proj4
-hfffy
+while True:
+    play = input('Start the game: ')
+    if play == "Yes":
+        text = str(input('Enter text in russian language: '))
+        letter = str(input('Enter letter: '))
+        v_letter = 'аеёиоуыэюяАЕЁИОУЫЭЮЯ'
+        c_letter = 'бвгджзйклмнпрстфхцчшщъьБВГДЖЗЙКЛМНПРСТФХЦЧШЩЪЬ'
+        signs = ' !"#$%&()*+,-./:;<=>?@[\]^_`{|}~'
+        word = []
+        translate = []
+        for symbol in text:
+            if symbol in signs:
+                word = symbol
+            elif symbol in v_letter:
+                word = symbol + letter + symbol.lower()
+            elif symbol in c_letter:
+                word = symbol
+            translate += word
+        print("".join(translate))
+    else:
+        print('Goodbye!!!')
+        break
